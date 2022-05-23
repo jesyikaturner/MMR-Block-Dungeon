3f6af01dc8b29016928c7daab5b4ffadd085d87e
Jessica Turner <jesyika@gmail.com>
2022-05-23 23:05:21 +1000

    gui is now connected to the dialouge manager, and changed up the screen resolution

--- Assets/Scripts/DialogueSystem/DialogueGUI.cs
+++ Assets/Scripts/DialogueSystem/DialogueGUI.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueGUI : MonoBehaviour
 {
-    private TextMesh _paragraph;
-    private TextMesh _name;
+    public TextMeshProUGUI _paragraph;
+    private TextMeshProUGUI _name;
     private Image _displayedImage;
+
+    private DialogueManager _dm;
 
     public void Start()
     {
-        _paragraph = GameObject.Find("Paragraph").GetComponent<TextMesh>();
-        _name = GameObject.Find("Name").GetComponent<TextMesh>();
+        _paragraph = GameObject.Find("Paragraph").GetComponent<TextMeshProUGUI>();
+        _name = GameObject.Find("Name").GetComponent<TextMeshProUGUI>();
         _displayedImage = GameObject.Find("Image").GetComponent<Image>();
+        _dm = GetComponent<DialogueManager>();
 
         if (!_displayedImage.sprite) 
             _displayedImage.gameObject.SetActive(false);
+
+        //_dm.SetCurrentDisplayedDialogue("shop_welcome");
+
+        //_paragraph.text = _dm.currDialogue.Paragraphs[0].Text;
+        //_name.text = _dm.currDialogue.Name;
+        //Debug.Log(_dm.GetKeyOfConnectedDialogue(2, 1));
     }
 
     public void StartDialogue(string key)
--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -7,18 +7,34 @@ public class DialogueManager: MonoBehaviour
     private DialogueList _dialogueList;
     public TextAsset dialogueJson;
 
+    public Dialogue currDialogue;
+    public int currentParagraph;
+
     public void Start()
     {
         _dialogueList = new DialogueList(dialogueJson);
+        currDialogue = null;
+    }
+    /// <summary>
+    /// This method gets the nextKey value of the choice object which links to the next dialogue object
+    /// if the user were to click on that dialogue option.
+    /// </summary>
+    /// <param name="currentPage"></param>
+    /// <param name="choiceID"></param>
+    /// <returns>Returns the key of the connected dialogue object.</returns>
+    public string GetKeyOfConnectedDialogue(int currentPage, int choiceID)
+    {
+        return currDialogue.Paragraphs[currentPage].Choices[choiceID].NextKey;
     }
 
-    public string GetNextKey(int currentPage, int choiceID)
+    public bool SetCurrentDisplayedDialogue(string key)
     {
-        return "";
+        currDialogue = _dialogueList.GetDialogueByKey(key);
+        return currDialogue != null;
     }
 
-    public Dialogue GetNextDialogue(string key)
-    {
-        return null;
-    }
+    //public Dialogue GetNextDialogue(string key)
+    //{
+    //    return _dialogueList.GetDialogueByKey(key);
+    //}
 }
